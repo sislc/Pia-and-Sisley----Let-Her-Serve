0e9376e0f7027a685c370dd6828e8d43ef80fb09
sislc <sisleytherese@gmail.com>
2024-05-17 12:19:37 -0400

    initial files

---
+++ MyWorld.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        playbutton playbutton = new playbutton();
+        addObject(playbutton,389,242);
+        girlmodel girlmodel = new girlmodel();
+        addObject(girlmodel,113,213);
+        title title = new title();
+        addObject(title,391,98);
+    }
+}
