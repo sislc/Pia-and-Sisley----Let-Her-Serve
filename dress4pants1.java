3b9c07bde72adc1a41768c82424be34d4c1617d5
sislc <sisleytherese@gmail.com>
2024-06-13 10:25:38 -0400

    updated fart

---
+++ dress4pants1.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class dress4pants1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class dress4pants1 extends Actor
+{
+    /**
+     * Act - do whatever the dress4pants1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
